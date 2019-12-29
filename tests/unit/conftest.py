38125b576938a56bb0e4f95625fde621e0d71ef8
Jumpei Yoshimra <jumpyoshim@gmail.com>
2019-12-29 20:36:02 +0900

    feat: add unit test

---
+++ tests/unit/conftest.py
@@ -0,0 +1,33 @@
+import os
+
+import boto3
+import moto
+import pytest
+
+os.environ['PETS_TABLE'] = 'pets'
+
+@pytest.fixture(autouse=True)
+def start_moto_mock():
+  moto.mock_dynamodb2().start()
+
+
+@pytest.fixture
+def create_pets_table():
+  region = os.getenv('AWS_DEFAULT_REGION', 'ap-northeast-1')
+  dynamodb = boto3.client('dynamodb', region_name=region)
+
+  dynamodb.create_table(
+    TableName=os.environ['PETS_TABLE'],
+    KeySchema=[
+      {
+        'AttributeName': 'id',
+        'KeyType': 'HASH'
+      }
+    ],
+    AttributeDefinitions=[
+      {
+        'AttributeName': 'id',
+        'AttributeType': 'N'
+      }
+    ]
+  )
